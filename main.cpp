f0103a5141689360ea5062b5802e1d18df4c8931
yangjaehoon <34858808+yangjaehoon@users.noreply.github.com>
2021-11-23 01:35:50 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,35 +9,35 @@ string choice[5] = { "환타", "사이다", "콜라", "파워에이드", "소주
 vector<pair<string, int>> mix;
 
 int main_menu() {
-	std::cout << "**************************************" << endl << endl;
-	std::cout << "1 : 직접 만드는 조합\n";
-	std::cout << "2 : 인기 순위\n";
-	std::cout << "3 : 이런 조합은 어떠세요?\n";
-	std::cout << "4 : 종료하기\n";
-	std::cout << "\n";
+	Serial.println("**************************************");
+	Serial.println("1 : 직접 만드는 조합");
+	Serial.println("2 : 인기 순위");
+	Serial.println("3 : 이런 조합은 어떠세요?");
+	Serial.println("4 : 종료하기");
+	Serial.println("");
 
 	int ret;
-	std::cout << "당신의 선택은 : ";
+	Serial.println("당신의 선택은 : ");
 	cin >> ret;
 
-	std::cout << endl << "**************************************" << endl << endl;
+	Serial.println("**************************************\n");
 
 	return ret;
 }
 
 int recomm_rate() {
-	std::cout << "**************************************" << endl << endl;
-	std::cout << "맛있게 드셨나요?" << endl;
-	std::cout << "체험해본 조합에 대해 이야기해주세요." << endl << endl;
+	Serial.println("**************************************\n");
+	Serial.println("맛있게 드셨나요?";
+	Serial.println("체험해본 조합에 대해 이야기해주세요.\n");
 
-	std::cout << "점수는 0점부터 9점까지 줄 수 있습니다!" << endl << endl;
+	Serial.println("점수는 0점부터 9점까지 줄 수 있습니다!\n");
 
 	int score;
-	std::cout << "당신의 점수는 : ";
+	Serial.println("당신의 점수는 : ");
 	std::cin >> score;
-	std::cout << endl << "점수 반영되었습니다." << endl << endl;
+	Serial.println("점수 반영되었습니다.");
 
-	std::cout << "**************************************" << endl << endl;
+	Serial.println("**************************************\n");
 
 	return score;
 }
@@ -48,41 +48,41 @@ void first_menu() {
 	int bev;
 	int ml;
 
-	std::cout << "**************************************" << endl;
+	Serial.println("**************************************\n");
 
 	while (1) {
-		std::cout << "어떤 음료를 드시겠습니까?" << endl << endl;
-		std::cout << "1. 환타 2. 사이다 3. 콜라 4. 파워에이드 5. 소주" << endl;
-		std::cout << "(*)을 누르면 종료됩니다." << endl << endl;
-		std::cout << "당신의 선택은 : ";
+		Serial.println( "어떤 음료를 드시겠습니까?\n");
+		Serial.println("1. 환타 2. 사이다 3. 콜라 4. 파워에이드 5. 소주\n");
+		Serial.println("(*)을 누르면 종료됩니다.\n");
+		Serial.println("당신의 선택은 : ");
 		std::cin >> input;
 
 		std::cout << endl;
 
 		if (input == '*') {
-			std::cout << "음료선택을 종료하겠습니다" << endl << endl;
+			Serial.println("음료선택을 종료하겠습니다");
 			break;
 		}
 
 		bev = int(input) - '0';	// char to int
 
 		if (bev < 1 || bev > 5) {
-			std::cout << "잘못된 입력입니다." << endl << endl;
+			Serial.println("잘못된 입력입니다.\n");
 			continue;
 		}
 
 		recipe_now += choice[bev-1] + " ";
 
 		std::cout << choice[bev-1] << "를 얼만큼 따르시겠습니까?" << endl << endl;
-		std::cout << "원하는 만큼 mL 단위로 입력해주세요! : ";
+		Serial.println("원하는 만큼 mL 단위로 입력해주세요! : ");
 		cin >> ml;
-		std::cout << endl << "**************************************" << endl << endl;
+		Serial.println("\n**************************************\n");
 		recipe_now += to_string(ml) + " ";
 	}
 
-	std::cout << "------------최종레시피-------------" << endl << endl;
+	Serial.println("------------최종레시피-------------\n");
 	cout << recipe_now << endl << endl;
-	std::cout << "-----------------------------------" << endl << endl;
+	Serial.println("-----------------------------------\n");
 
 	//pour_juice(recipe_now); 여기서부터 붓기 시작함
 
@@ -95,23 +95,23 @@ bool cmp(pair<string, int> a, pair<string, int> b) {
 }
 
 void second_menu() {
-	std::cout << "**************************************" << endl << endl;
+	Serial.println("**************************************");
 
 	sort(mix.begin(), mix.end(), cmp);
 
-	std::cout << "이런 조합이 있어요\n\n";
+	Serial.println("이런 조합이 있어요\n\n");
 
 	for (int i = 0; i < mix.size(); i++)
 		std::cout << i+1 << "  |  " << mix[i].first << "\n\n";
 
-	std::cout << "원하는 조합을 선택해주세요\n\n";
+	Serial.println("원하는 조합을 선택해주세요\n\n");
 
 	int choice;
-	std::cout << "당신의 조합은? : ";
+	Serial.println("당신의 조합은? : ");
 	std::cin >> choice;
 
 	std::cout << "\n" << mix[choice-1].first << " 조합을 선택하였습니다.\n";
-	std::cout << "제조 시작합니다...\n\n";
+	Serial.println("제조 시작합니다...\n\n");
 
 	//여기서부터 이제 붓기 시작함
 	//pour_juice(mix[choice-1].first);
@@ -121,7 +121,7 @@ void second_menu() {
 }
 
 void third_menu() {
-	std::cout << "**************************************" << endl << endl;
+	Serial.println("**************************************\n");
 	std::cout << "이런 <증명된> 조합도 있어요" << endl << endl;
 
 	string good_mix[3] = { "사이다 20 파워에이드 20 소주 40", "콜라 30 소주 50", "사이다 70 소주 10" };
@@ -130,18 +130,18 @@ void third_menu() {
 		std::cout << i + 1 << "  |  " << good_mix[i] << endl << endl;
 	}
 
-	std::cout << "당신의 선택은? : ";
+	Serial.println("당신의 선택은? : ");
 	int choice;
 	std::cin >> choice;
 
 	//여기서부터 이제 붓기 시작함
 	//pour_juice(good_mix[choice-1]);
 
-	std::cout << "**************************************" << endl << endl;
+	Serial.println("**************************************\n");
 }
 
 void pour_juice(string str) {
-	std::cout << "**************************************" << endl << endl;
+	Serial.println("**************************************\n");
 	std::cout << str << "의 조합을 만들고 있습니다......" << endl << endl;
 	string temp;
 	bool check_type = 1;	// 1이면 음료수, 0이면 ml로 하자
@@ -167,7 +167,7 @@ void pour_juice(string str) {
 		}
 	}
 
-	std::cout << "완성되었습니다." << endl << endl;
+	Serial.println("완성되었습니다.\n");
 }
 
 int main() {
@@ -188,10 +188,10 @@ int main() {
 			third_menu();	// 세 번째 메뉴 진입
 			break;
 		case 4:
-			std::cout << "**************************************" << endl << endl;
-			std::cout << "프로그램 종료합니다." << endl << endl;
-			std::cout << "이용해주셔서 감사합니다." << endl << endl;
-			std::cout << "**************************************" << endl << endl;
+			Serial.println("**************************************\n");
+			Serial.println("프로그램 종료합니다.\n");
+			Serial.println("이용해주셔서 감사합니다.\n");
+			Serial.println("**************************************\n");
 			checkFinish = 1;
 			break;
 		}
